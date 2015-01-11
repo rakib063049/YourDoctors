bc2cef914a3a757522eb6a02aa07b051b392ee78
rakib hasan <rakib063049@gmail.com>
2015-01-11 23:57:48 +0600

    Fix# mange hospital for authority

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,17 +24,39 @@ module ApplicationHelper
     end
   end
 
-  def hospital_edit_link(hospital)
-    if resource_owner?(hospital)
-      link_to edit_hospital_path(hospital), class: 'btn btn-primary btn-xs' do
+  def hospital_edit_link(resource)
+    if resource_owner?(resource)
+      link_to edit_hospital_path(resource), class: 'btn btn-primary btn-xs' do
         "<i class='glyphicon glyphicon-wrench'></i> Edit".html_safe
       end
     end
   end
 
-  def hospital_destroy_link(hospital)
-    if resource_owner?(hospital)
-      link_to hospital_path(hospital), method: :delete, data: {confirm: 'Are you sure?'}, class: 'btn btn-danger btn-xs' do
+  def hospital_destroy_link(resource)
+    if resource_owner?(resource)
+      link_to hospital_path(resource), method: :delete, data: {confirm: 'Are you sure?'}, class: 'btn btn-danger btn-xs' do
+        "<i class='glyphicon glyphicon-trash'></i> Delete".html_safe
+      end
+    end
+  end
+
+  def hospital_authority_new_link
+    link_to new_hospital_authority_path, class: 'btn btn-primary btn-xs' do
+      "<i class='glyphicon glyphicon-plus'></i> Add New".html_safe
+    end
+  end
+
+  def hospital_authority_edit_link(resource)
+    if resource_owner?(resource)
+      link_to edit_hospital_authority_path(resource), class: 'btn btn-primary btn-xs' do
+        "<i class='glyphicon glyphicon-wrench'></i> Edit".html_safe
+      end
+    end
+  end
+
+  def hospital_authority_destroy_link(resource)
+    if resource_owner?(resource)
+      link_to hospital_authority_path(resource), method: :delete, data: {confirm: 'Are you sure?'}, class: 'btn btn-danger btn-xs' do
         "<i class='glyphicon glyphicon-trash'></i> Delete".html_safe
       end
     end
